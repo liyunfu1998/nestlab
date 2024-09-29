8f83165a06ec8a965d33ed4cd2755eaf3f3d4641
liyunfu1998 <liyunfu1998@qq.com>
2024-09-29 04:58:44 +0000

    feat: 实现登录注册

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,19 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
+  @Post('register')
   register(@Body() registerUserDto: RegisterUserDto) {
-    console.log(registerUserDto);
-    return 'done';
+    return this.userService.register(registerUserDto);
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    return this.userService.login(loginUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { DbService } from 'src/db/db.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { User } from './entities/user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  @Inject(DbService)
+  dbService: DbService;
+
+  async register(registerUserDto: RegisterUserDto) {
+    const users: User[] = await this.dbService.read();
+
+    const foundUser = users.find(
+      (item) => item.username === registerUserDto.username,
+    );
+
+    if (foundUser) {
+      throw new BadRequestException('该用户已注册');
+    }
+
+    const user = new User();
+
+    user.username = registerUserDto.username;
+    user.password = registerUserDto.password;
+    users.push(user);
+
+    await this.dbService.write(users);
+
+    return user;
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const users: User[] = await this.dbService.read();
+
+    const foundUser = users.find(
+      (item) => item.username === loginUserDto.username,
+    );
+
+    if (!foundUser) {
+      throw new BadRequestException('该用户不存在');
+    }
+
+    if (foundUser.password !== loginUserDto.password) {
+      throw new BadRequestException('密码错误');
+    }
+
+    return foundUser;
+  }
+}
