edccbf9286feb3ca68cf53c2ad8d907f139800de
liyunfu1998 <liyunfu1998@qq.com>
2024-09-29 04:41:37 +0000

    feat: 添加body校验管道

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // 启用全局验证管道
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   // 设置全局前缀
   app.setGlobalPrefix('api');
