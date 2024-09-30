d17bd211f1d90e82cd2e1f503542533cc8ba6977
liyunfu1998 <liyunfu1998@qq.com>
2024-09-30 02:24:20 +0000

    feat: 添加局部中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DbModule } from './db/db.module';
 import { BookModule } from './book/book.module';
+import { LogMiddleware } from './log.middleware';
 
 @Module({
   imports: [UserModule, DbModule, BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LogMiddleware).forRoutes('aaa*');
+  }
+}
